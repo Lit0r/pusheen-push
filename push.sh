13583fbca18e21ffc16b018c0ae433de80dbeb36
Lit0r <elliotr@andrew.cmu.edu>
2015-07-08 22:36:56 -0400

    new pusheen, new pusheen extension, randomization

--- push.sh
+++ push.sh
@@ -3,15 +3,25 @@
 # Elliot Rosen 2015.
 # I don't own Pusheen, Pusheen Corp. does.
 # TODO liscence stuff
-command=$1
-result=$($command push ${@:2})
-pstatus=$?
-echo -e $result
-pusheen='p1'
+
+
+
+#command=$1
+#result=$($command push ${@:2})
+#pstatus=$?
+#echo -e $result
+
+
+this=$( cd "$(dirname "$0")" ; pwd -P )
+
+# don't do sneaky filenmes and all will be fine.
+cats=($(ls "$this/pusheens/"*.nya))
+pusheen=${cats[$RANDOM % ${#cats[@]} ]}
+
 
 # It'd be a crime not to use 'cat' here
-if [ $pstatus -eq 0 ] ; then
-    cat "pusheens/$pusheen.txt"
-fi
+#if [ $pstatus -eq 0 ] ; then
+    cat $pusheen
+#fi
 
 # :3
